8f50a8757a83047d83fe190d739df6e33aed161d
m-ray-dev <monovray.devs@gmail.com>
2026-01-11 16:04:55 +0530

    gin

---
+++ golang tutorials/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello  world")
+}
+
+// This is a comment
+/* The code below will print Hello World
+to the screen, and it is amazing */
